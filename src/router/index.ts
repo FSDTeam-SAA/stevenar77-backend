dfa459e7379909bbbf05d05e115b0a7895f1e638
zihadulislam2 <zihadul708@gmail.com>
2025-09-10 05:25:13 +0600

    update src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,23 +2,28 @@ import { Router } from "express";
 import userRouter from "../modules/user/user.router";
 import authRouter from "../modules/auth/auth.router";
 import classRouter from "../modules/class/class.router";
+import classBookingRouter from "../modules/bookingClass/bookingClass.routes";
 
 const router = Router();
 
 const moduleRoutes = [
   {
-    path: "/user",
+    path: '/user',
     route: userRouter,
   },
   {
-    path: "/auth",
+    path: '/auth',
     route: authRouter,
   },
   {
-    path: "/class",
+    path: '/class',
     route: classRouter,
-  }
-];
+  },
+  {
+    path: '/class/bookings',
+    route: classBookingRouter,
+  },
+]
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
