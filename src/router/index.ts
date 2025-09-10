d35d03f8a50178a062213eddf50fa636ee8d8581
Sajjad Hossain <155253869+Sajjad177@users.noreply.github.com>
2025-09-11 01:18:17 +0600

    issue fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import userRouter from "../modules/user/user.router";
 import authRouter from "../modules/auth/auth.router";
-import classRouter from "../modules/class/class.router";
 import courseRouter from "../modules/course/course.router";
 
 const router = Router();
@@ -14,10 +13,6 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: authRouter,
-  },
-  {
-    path: "/class",
-    route: classRouter,
   },
   {
     path: "/course",
