f19b54931ed3dcf25eb3902bcb85ce4247c5ab9d
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-09-19 08:50:52 +0600

    update

--- src/modules/product/product.interface.ts
+++ src/modules/product/product.interface.ts
@@ -1,14 +1,14 @@
-// export interface IProduct {
-//   _id: string;
-//   title: string;
-//   shortDescription: string;
-//   category: string;
-//   price: number;
-//   images: { public_id: string; url: string }[];
-//   longDescription: string;
-//   inStock: boolean;
-//   featured: string[];
-//   totalReviews: number;
-//   averageRating: number;
-//   quantity: number;
-// }
+export interface IProduct {
+  _id: string;
+  title: string;
+  shortDescription: string;
+  category: string;
+  price: number;
+  images: { public_id: string; url: string }[];
+  longDescription: string;
+  inStock: boolean;
+  featured: string[];
+  totalReviews: number;
+  averageRating: number;
+  quantity: number;
+}
--- src/modules/product/product.model.ts
+++ src/modules/product/product.model.ts
@@ -1,31 +1,31 @@
-// import { model, Schema } from "mongoose";
-// import { IProduct } from "./product.interface";
+import { model, Schema } from "mongoose";
+import { IProduct } from "./product.interface";
 
-// const productSchema = new Schema<IProduct>(
-//   {
-//     title: { type: String, required: true },
-//     shortDescription: { type: String, required: true },
-//     price: { type: Number, required: true },
-//     images: [
-//       {
-//         public_id: { type: String },
-//         url: { type: String },
-//         _id: false,
-//       },
-//     ],
-//     category: { type: String, required: true },
-//     longDescription: { type: String, required: true },
-//     inStock: { type: Boolean, default: true },
-//     featured: { type: [String], default: [] },
-//     totalReviews: { type: Number, default: 0 },
-//     averageRating: { type: Number, default: 0 },
-//     quantity: { type: Number, required: true },
-//   },
-//   {
-//     timestamps: true,
-//     versionKey: false,
-//   }
-// );
+const productSchema = new Schema<IProduct>(
+  {
+    title: { type: String, required: true },
+    shortDescription: { type: String, required: true },
+    price: { type: Number, required: true },
+    images: [
+      {
+        public_id: { type: String },
+        url: { type: String },
+        _id: false,
+      },
+    ],
+    category: { type: String, required: true },
+    longDescription: { type: String, required: true },
+    inStock: { type: Boolean, default: true },
+    featured: { type: [String], default: [] },
+    totalReviews: { type: Number, default: 0 },
+    averageRating: { type: Number, default: 0 },
+    quantity: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
-// const Product = model<IProduct>("Product", productSchema);
-// export default Product;
+const Product = model<IProduct>("Product", productSchema);
+export default Product;
