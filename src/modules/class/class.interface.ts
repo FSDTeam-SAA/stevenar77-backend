c9514b3bfc79e868fd759f2b74b4a6438698caf7
zihadulislam2 <zihadul708@gmail.com>
2025-09-24 03:59:00 +0600

    update class.interface.ts, class.model.ts and src/router/index.ts

--- src/modules/class/class.interface.ts
+++ src/modules/class/class.interface.ts
@@ -1,13 +1,14 @@
 export interface IClass {
-  title: string;
-  image: { public_id: string; url: string };
-  description?: string;
-  price: number[];
-  courseIncludes: string[];
-  duration: string;
-  totalReviews?: number;
-  avgRating?: number;
-  participates: number;
-  totalParticipates: number;
-  isActive?: boolean;
+  title: string
+  image: { public_id: string; url: string }
+  description?: string
+  price: number[]
+  courseIncludes: string[]
+  duration: string
+  totalReviews?: number
+  avgRating?: number
+  participates: number
+  totalParticipates: number
+  isActive?: boolean
+  classDates?: Date
 }
