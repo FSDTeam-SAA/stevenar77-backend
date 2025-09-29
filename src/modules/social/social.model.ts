bb6acb0aa20efe378b647d9de2df0f591292f52b
zihadulislam2 <zihadul708@gmail.com>
2025-09-30 01:47:13 +0600

    feat create social apis

---
+++ src/modules/social/social.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema, Document } from 'mongoose'
+
+export interface ISocial extends Document {
+  facebook: string
+  instagram: string
+  location: string
+  email: string
+}
+
+const SocialSchema: Schema = new Schema(
+  {
+    facebook: {
+      type: String,
+      required: true,
+    },
+    instagram: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // Make email unique
+    },
+  },
+  { timestamps: true }
+)
+
+const Social = mongoose.model<ISocial>('Social', SocialSchema)
+
+export default Social
